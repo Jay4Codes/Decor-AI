6e36b59f8dca3c39a8441eb81a7b63ca6f773433
jay4codes <jay4codes@gmail.com>
2023-04-08 17:40:16 +0530

    react app migarated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React from "react";
-// import Replicate from "replicate";
+import "./App.css";
 
-const RoomGPT = () => {
-  // const replicate = new Replicate({
-  //   auth: "a4d8b9cece7fec7ab285af80af02020910dcfe98",
-  // });
-
-  const startResponse = fetch("https://api.replicate.com/v1/predictions", {
+function App() {
+  let startResponse = fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
       Authorization: "Token a4d8b9cece7fec7ab285af80af02020910dcfe98",
     },
     body: JSON.stringify({
@@ -18,8 +12,8 @@ const RoomGPT = () => {
         "854e8727697a057c525cdb45ab037f64ecca770a1769cc52287c2e56472a247b",
       input: {
         image:
-          "https://hips.hearstapps.com/hmg-prod/images/harder-jett-008-1643379018.jpg",
-        prompt: "a video gaming room",
+          "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/612QHteoZ2S._SY450_.jpg",
+        prompt: "hello world",
         scale: 9,
         a_prompt:
           "best quality, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning, interior design, natural lighting",
@@ -30,7 +24,8 @@ const RoomGPT = () => {
   });
 
   console.log(startResponse);
-  return <div>Replicate</div>;
-};
 
-export default RoomGPT;
+  return <div className="App"></div>;
+}
+
+export default App;
